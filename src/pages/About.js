1d060aaae7aff3987214686b09b0496f5a885c0f
BowlerM <mattbow55@googlemail.com>
2025-05-11 18:41:15 +0100

    updated cv and about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,13 +6,12 @@ const About = () => {
     return (
         <div className="about-content">
             <h2>About Me</h2>
-            <p>I'm a 3rd year Computer Science student studying for a MEng at The University of Leeds. I'm passionate
+            <p>I'm a 3rd year Computer Science student studying for a BSc at The University of Leeds. I'm passionate
                 about software development with most experience in web development
                 but always eager to expand my skills into other areas of software engineering.
             </p>
 
-            <p>I'm currently seeking a placement year for in between my 3rd and 4th year of university but also open
-                to a summer internship.
+            <p>I'm currently seeking a graduate position in the software industry.
             </p>
             
             <p>Please use the sections on the sidebar to learn more about me and my work.</p>
